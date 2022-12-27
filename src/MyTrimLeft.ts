5f3e32e312314eee68360f85a257663ab3ade826
Rich4st <zmingcheng204@gmail.com>
2022-12-27 10:42:49 +0800

    doc: MyTrimLeft

--- src/MyTrimLeft.ts
+++ src/MyTrimLeft.ts
@@ -1,9 +1,15 @@
 const foo = '    Hello World!   '
 
+/**
+ * https://github.com/type-challenges/type-challenges/blob/main/questions/00106-medium-trimleft/README.zh-CN.md
+ * @description remove left spaces
+ * @example type result = MyTrimLeft<typeof foo>
+ */
+
 type MyTrimLeft<T> = T extends `${infer L}${infer R}`
   ? L extends ' '
     ? MyTrimLeft<R>
     : T
   : T
 
-type result = MyTrimLeft<typeof foo>
+type result = MyTrimLeft<typeof foo> // expected `Hello World!   `
