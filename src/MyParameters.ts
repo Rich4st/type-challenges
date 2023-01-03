e5fbd7d2e76013fce6dc9396f1a2938a41cce5aa
Rich4st <zmingcheng204@gmail.com>
2023-01-03 12:47:47 +0800

    feat: MyParameters

---
+++ src/MyParameters.ts
@@ -0,0 +1,11 @@
+const foo = (_arg1: string, _arg2: number): void => {}
+
+/**
+ * https://github.com/type-challenges/type-challenges/blob/main/questions/03312-easy-parameters/README.zh-CN.md
+ * @description 实现一个通用的 MyParameters<T>，它接受一个函数类型 T 作为参数，返回 T 的参数类型组成的元组类型。
+ * @example type result = MyParameters<typeof foo> // expected to be ["string", number]
+ */
+
+type MyParameters<T> = T extends (...args: infer P) => any ? P : never
+
+type result = MyParameters<typeof foo> // expected to be ["string", number]
